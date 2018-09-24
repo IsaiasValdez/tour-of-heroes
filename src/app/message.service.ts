a0febec9bc42fab3921009bf2e03d15eb0aafa29
Isaias Valdez <isaias.arr.valdez@gmail.com>
2018-09-23 22:47:54 -0500

    Added a message service that display at the bottom of the page

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class MessageService {
+  messages: string[] = [];
+
+  add(message: string): void {
+    this.messages.push(message);
+  }
+
+  clear(): void {
+    this.messages = [];
+  }
 
   constructor() { }
 }
